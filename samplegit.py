ff6023a9c595ad6db53ffdfa2ad89adddb7f3538
“Arun20” <queenprince20@gmail.com>
2022-04-28 02:30:15 +0000

    thirdchange

--- samplegit.py
+++ samplegit.py
@@ -1,12 +1,12 @@
 # Creating a Dictionary
 # with Integer Keys
 Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
-print("\nDictionary with the use of Integer Keys: ")
+print("\nDictionary with use of Integer Keys: ")
 print(Dict)
 
 # Creating a Dictionary
 # with Mixed keys
-Dict = {'Name': 'Geeks', 1: [1, 2, 3]}
+Dict = {'Name': 'Geeks', 1: [1, 2,]}
 print("\nDictionary with the use of Mixed Keys: ")
 print(Dict)
 
